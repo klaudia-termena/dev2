a11d8430690587fa4f3d5f441fe1bcc7192a9a5e
Klaudia Termena <klaudia.termena@gmail.com>
2020-06-14 19:49:42 +0200

    changes

---
+++ main.js
@@ -0,0 +1,5 @@
+const introduction = "This is my first website ever";
+const followUp = "Enjoy!";
+console.log(introduction);
+console.log(followUp);
+console.log(`Hello, my name is Klaudia. ${introduction}.${followUp}`)
